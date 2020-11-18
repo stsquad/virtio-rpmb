b5a15fc22df21e5a1b265f3ee21283a6ae474dad
Alex Bennée <alex.bennee@linaro.org>
2020-11-18 11:30:21 +0000

    add cli arguments
    The declaration alone can't specify all the inter-dependencies. We
    will at least need to verify flash_path is set if we are actually
    going to run.

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,14 @@
  * (C)opyright 2020 Linaro
  * Author: Alex Bennée <alex.bennee@linaro.org>
  */
+
+#[macro_use]
+extern crate clap;
+use clap::App;
+
 fn main() {
+    let yaml = load_yaml!("cli.yaml");
+    let cmd_args = App::from_yaml(yaml).get_matches();
+
     println!("Hello, world!");
 }
