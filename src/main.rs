e10250a65d7cc0fdc64ff1510ec8fca804cc91f6
Alex Bennée <alex.bennee@linaro.org>
2020-11-18 12:31:09 +0000

    implement --print-capabilities

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,16 @@
 extern crate clap;
 use clap::App;
 
+use std::process::exit;
+
 fn main() {
     let yaml = load_yaml!("cli.yaml");
     let cmd_args = App::from_yaml(yaml).get_matches();
 
-    println!("Hello, world!");
+    if cmd_args.is_present("print_cap") {
+        println!("{{");
+        println!("  \"type\": \"block\"");
+        println!("}}");
+        exit(0);
+    }
 }
